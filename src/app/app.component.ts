dac5c85c7f325d8b72407d879f1cac0b065ef4c9
Ahmed Eleojo Musa <w0866236@myscc.ca>
2024-09-16 15:35:04 -0400

    Created starter Angular file and completed Assignment 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,6 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'Ahmed-Musa-Learning-Angular';
+  province:string = 'Nova Scotia';
+  favoriteHoliday:string = 'Bern, Switzerland';
 }
