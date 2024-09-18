ec774c3c87521871603ec03720a1610732b301da
Ahmed Eleojo Musa <w0866236@myscc.ca>
2024-09-18 10:57:36 -0400

    Completed assignment 2 by doing the html and css files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
+import {CommonModule} from "@angular/common";
 import { RouterOutlet } from '@angular/router';
+import {User} from "./models/user";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Ahmed-Musa-Learning-Angular';
-  province:string = 'Nova Scotia';
-  favoriteHoliday:string = 'Bern, Switzerland';
+  users: User[] = [
+    { id: 1, name: 'HP Stream', price: 1250, description: 'Quality meets sleekness', isAvailable: true },
+    { id: 2, name: 'HP Probook', price: 650, description: 'Efficiency for less price', isAvailable: true },
+    { id: 3, name: 'HP Premium', price: 389, isAvailable: false },
+    { id: 4, name: 'HP Elitebook', price: 934, description: 'Rugged and affordable', isAvailable: true },
+    { id: 5, name: 'HP Folio', price: 693, description: 'Work and play', isAvailable: true },
+    { id: 6, name: 'HP Chromebook', price: 720, description: 'Smaller is better', isAvailable: false }
+  ];
 }
