3ec8168feacfd57ff8d801cb67f293bd2ec674ab
Ahmed Eleojo Musa <w0866236@myscc.ca>
2024-10-09 15:25:22 -0400

    Defined a list of routes

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import {provideRouter, Routes} from "@angular/router";
+//import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {ProductListComponent} from "./app/product-list/product-list.component";
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+// Define list of routes
+const routes: Routes = [
+  {path:'', redirectTo: '/products', pathMatch: 'full'}, //default route
+  { path: 'products', component: ProductListComponent }  // Main route for product list
+];
+
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes)]
+}).then(() => console.log('Bootstrap successful'))
+  .catch(err => console.error('Bootstrap failed', err));
