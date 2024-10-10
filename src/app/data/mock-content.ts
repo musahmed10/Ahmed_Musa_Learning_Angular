4e7d6109db3a8d6e4c3a8bbf915993a865d3d8d0
Ahmed Eleojo Musa <w0866236@myscc.ca>
2024-10-09 20:02:18 -0400

    assets folder added

--- src/app/data/mock-content.ts
+++ src/app/data/mock-content.ts
@@ -3,14 +3,15 @@ export interface Product {
   price: number;
   description?: string; // Optional property
   isAvailable: boolean;
+  imageUrl?: string;
 }
 
 export const products: Product[] = [
-  { name: 'Sample Product 2', price: 49.99, description: 'This is a sample product 2.', isAvailable: false },
-  { name: 'HP Stream', price: 1250, description: 'Quality meets sleekness', isAvailable: true },
-  { name: 'HP Probook', price: 650, description: 'Efficiency for less price', isAvailable: true },
-  { name: 'HP Premium', price: 389, isAvailable: false },
-  { name: 'HP Elitebook', price: 934, description: 'Rugged and affordable', isAvailable: true }
+  { name: 'Sample Product 2', price: 49.99, description: 'This is a sample product 2.', isAvailable: false, imageUrl: 'assets/images/sample.jpeg'},
+  { name: 'HP Stream', price: 1250, description: 'Quality meets sleekness', isAvailable: true, imageUrl: 'assets/images/stream.jpeg' },
+  { name: 'HP Probook', price: 650, description: 'Efficiency for less price', isAvailable: true, imageUrl: 'assets/images/probook.jpeg' },
+  { name: 'HP Premium', price: 389, isAvailable: false, imageUrl: 'assets/images/premium.jpeg' },
+  { name: 'HP Elitebook', price: 934, description: 'Rugged and affordable', isAvailable: true, imageUrl: 'assets/images/elitebook.jpeg' }
 
 ];
 
