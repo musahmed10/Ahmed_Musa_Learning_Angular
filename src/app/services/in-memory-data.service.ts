337faab0558ceed3387debe01a393834f25e99ba
Ahmed Eleojo Musa <w0866236@myscc.ca>
2024-11-03 20:21:48 -0500

    Installed in-memory-web-api and created in-memory-data.service.ts

---
+++ src/app/services/in-memory-data.service.ts
@@ -0,0 +1,18 @@
+import {InMemoryDbService} from "angular-in-memory-web-api";
+import {Product} from "../data/mock-content";
+
+
+export class InMemoryDataService implements InMemoryDbService {
+  createDb():{products: Product[]} {
+    const products: Product[] = [
+      { name: 'Sample Product 2', price: 49.99, description: 'This is a sample product 2.', isAvailable: false, imageUrl: 'assets/images/sample.jpeg'},
+      { name: 'HP Stream', price: 1250, description: 'Quality meets sleekness', isAvailable: true, imageUrl: 'assets/images/stream.jpeg' },
+      { name: 'HP Probook', price: 650, description: 'Efficiency for less price', isAvailable: true, imageUrl: 'assets/images/probook.jpeg' },
+      { name: 'HP Premium', price: 389, isAvailable: false, imageUrl: 'assets/images/premium.jpeg' },
+      { name: 'HP Elitebook', price: 934, description: 'Rugged and affordable', isAvailable: true, imageUrl: 'assets/images/elitebook.jpeg' }
+
+    ];
+    return { products };
+
+  }
+}
