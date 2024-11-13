c7caf97fe2a7d074451d807fca5ca214a81b8cfe
Ahmed Eleojo Musa <w0866236@myscc.ca>
2024-11-13 17:32:47 -0500

    Implemented lazy loading for modify product page and Error 404 page.

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,20 @@ import {provideRouter, Routes} from "@angular/router";
 //import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import {ProductListComponent} from "./app/product-list/product-list.component";
-import {PageNotFoundComponent} from "./app/page-not-found/page-not-found.component";
-import {ModifyListItemComponent} from "./app/modify-list-item/modify-list-item.component";
+//import {PageNotFoundComponent} from "./app/page-not-found/page-not-found.component";
+//import {ModifyListItemComponent} from "./app/modify-list-item/modify-list-item.component";
 
 
 // Define list of routes
 const routes: Routes = [
   {path:'', redirectTo: '/products', pathMatch: 'full'}, //default route
-  { path: 'products', component: ProductListComponent },  // Main route for product list
-  {path:'modify-product', component: ModifyListItemComponent},
-  {path:'**', component: PageNotFoundComponent} // Will serve ass my 404 pge
+  { path: 'products', component: ProductListComponent },  // Eagerly loaded // Main route for product list
+  //{path:'modify-product', component: ModifyListItemComponent}, // previously eagerly loaded
+  {path:'modify-product', loadComponent: () =>
+      import('./app/modify-list-item/modify-list-item.component').then(m => m.ModifyListItemComponent)},// Now lazy loaded
+  //{path:'**', component: PageNotFoundComponent} // Will serve ass my 404 pge // previously eagerly loaded
+  {path:'**', loadComponent: () =>
+      import('./app/page-not-found/page-not-found.component').then(m => m.PageNotFoundComponent)},// Now lazy loaded
 ];
 
 bootstrapApplication(AppComponent, {
