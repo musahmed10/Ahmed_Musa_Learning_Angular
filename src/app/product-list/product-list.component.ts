c4ea6965acf95345773afdb7c1cc066b5572bc6a
Ahmed Eleojo Musa <w0866236@myscc.ca>
2024-11-19 20:30:31 -0500

    Implemented the HoverHighlight Directive with a grey color when a user hovers each product item on the list.

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -4,14 +4,15 @@ import { ProductListItemComponent } from '../product-list-item/product-list-item
 import { Observable } from 'rxjs';
 import { ProductService } from '../services/product.service';
 import { Product } from '../data/mock-content';
-import { Router } from '@angular/router';
+import {Router, RouterLink} from '@angular/router';
 import {FiftyPercentPromoPipe} from "../pipes/fifty-percent-promo.pipe";
 import {AvailabilityColorsPipe} from "../pipes/availability-colors.pipe";
+import {HoverHighlightDirective} from "../directives/hover-highlight.directive";
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [CommonModule, ProductListItemComponent, FiftyPercentPromoPipe, AvailabilityColorsPipe],
+  imports: [CommonModule, ProductListItemComponent, FiftyPercentPromoPipe, AvailabilityColorsPipe, HoverHighlightDirective, RouterLink],
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
